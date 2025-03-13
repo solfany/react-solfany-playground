488fcf589926b347915a77d340abbc6a0c926544
DESKTOP-9K23CHS\np <solfany1999@gmail.com>
2025-03-14 08:32:55 +0900

    refactor: 배경 url 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="background">
          <video className="video-bg" autoPlay loop muted playsInline>
-        <source src="/videos/main.mp4" type="video/mp4" />
+        <source src="https://mylivewallpapers.com/wp-content/uploads/Anime/PREVIEW-Ai-Hoshino.mp4" type="video/mp4" />
         당신의 브라우저는 HTML5 비디오를 지원하지 않습니다.
       </video>
       <div className="overlay"></div>
